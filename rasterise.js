5cc16733597f4597294961b0357351491f9147c5
Sean Ho <github@seanho.com>
2016-09-08 19:28:23 -0700

    don't lint rasterise.js

--- rasterise.js
+++ rasterise.js
@@ -1,7 +1,6 @@
 // Derived from phantomjs example rasterize.js
 // https://github.com/ariya/phantomjs/blob/master/examples/rasterize.js
 
-"use strict";
 var page = require('webpage').create(),
     system = require('system'),
     address, output, paper, pageW, pageH, size;
@@ -15,7 +14,7 @@ if (system.args.length < 3 || system.args.length > 5) {
 } else {
     address = system.args[1];
     output = system.args[2];
-    paper = "1024px*768px";
+    paper = '1024px*768px';
     pageW = '1024px';
     pageH = '768px';
     if (system.args.length > 3) {
@@ -27,24 +26,24 @@ if (system.args.length < 3 || system.args.length > 5) {
         pageW = parseInt(size[0], 10);
         pageH = parseInt(size[1], 10);
     } else if (output.substr(-4) === ".pdf") {
-        console.log("PDF paper size:", paper);
+        console.log('PDF paper size:', paper);
         page.paperSize = { format: paper, orientation: 'portrait', margin: '1cm' };
     } else {
         pageW = parseInt(size, 10);
         pageH = Math.round(pageW * 3/4);
     }
 
-    console.log("Page size:", pageW, "x", pageH);
+    console.log('Page size:', pageW, 'x', pageH);
     page.paperSize = { width: pageW, height: pageH, margin: '0px' };
     page.viewportSize = { width: pageW, height: pageH };
     page.clipRect = { top: 0, left: 0, width: pageW, height: pageH };
 
     if (system.args.length > 4) {
         page.zoomFactor = system.args[4];
-        console.log("Zoom:", page.zoomFactor);
+        console.log('Zoom:', page.zoomFactor);
     }
 
-    console.log("Wait 5 sec for render...");
+    console.log('Wait 5 sec for render...');
     page.open(address, function (status) {
         if (status !== 'success') {
             console.log('Unable to load the address!');
