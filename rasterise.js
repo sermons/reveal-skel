cda2fd4495cb9ce6463f78a291c92aa03515f353
Sean Ho <github@seanho.com>
2016-10-12 23:46:22 -0700

    Strip out fancy processing of PDF paper formats

--- rasterise.js
+++ rasterise.js
@@ -5,43 +5,26 @@ var page = require('webpage').create(),
     system = require('system'),
     address, output, paper, pageW, pageH, size;
 
-if (system.args.length < 3 || system.args.length > 5) {
-    console.log('Usage: rasterize.js URL filename [width[*height]|format] [zoom]');
-    console.log('  dims: e.g., "1024px*768px" (default), "1920px*1080px", "800px" (4:3 aspect)');
-    console.log('  PDF paper format: e.g., "10cm*15cm", "Letter", "A4"');
+if (system.args.length < 3 || system.args.length > 6) {
+    console.log('Usage: rasterize.js URL filename [width] [height] [zoom]');
+    console.log('  e.g., http://example.com output.pdf 1024 768 1.0');
     phantom.exit(1);
 } else {
+    pageW = 1024;
+    pageH =  768;
     address = system.args[1];
     output = system.args[2];
-    size = '1024px*768px';
-    if (system.args.length > 3) size = system.args[3];
+    if (system.args.length > 3) pageW = system.args[3];
+    if (system.args.length > 4) pageH = system.args[4];
+    if (system.args.length > 5) page.zoomFactor = system.args[5];
 
-    ispdf = output.substr(-4) === ".pdf"
-    isdim = size.indexOf('*') > -1
-
-    if (ispdf && !isdim) {
-        console.log('PDF paper format:', size);
-        page.paperSize = { format: size, orientation: 'portrait', margin: '1cm' };
-    } else {
-        pageW = 0
-        pageH = 0
-        if (isdim) {
-            dims = size.split('*');
-            pageW = parseInt(dims[0], 10);
-            pageH = parseInt(dims[1], 10);
-        } else {
-            pageW = parseInt(size, 10);
-            pageH = Math.round(pageW * 3/4);
-        }
-        console.log('Page size:', pageW, 'x', pageH);
-        page.viewportSize = { width: pageW, height: pageH };
+    page.viewportSize = { width: pageW, height: pageH };
+    page.clipRect = { top: 0, left: 0, width: pageW, height: pageH };
+    if (output.substr(-4) === ".pdf") {
+        page.paperSize = { width: pageW, height: pageH, margin: 0 };
     }
 
-    if (system.args.length > 4) {
-        page.zoomFactor = system.args[4];
-        console.log('Zoom:', page.zoomFactor);
-    }
-
+    console.log('Saving', address, 'to', output, 'at', pageW, 'x', pageH, 'zoom', page.zoomFactor);
     console.log('Wait 10 sec for render...');
     page.open(address, function (status) {
         if (status !== 'success') {
