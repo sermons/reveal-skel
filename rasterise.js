19cdde2a33d9d3f1342121965ebfd685a569bf1f
Sean Ho <github@seanho.com>
2016-10-15 17:12:24 -0700

    tidy local variable declaration

--- rasterise.js
+++ rasterise.js
@@ -3,9 +3,7 @@
 
 var page = require('webpage').create(),
     system = require('system'),
-    address, output, pageW, pageH;
-
-var Usage =
+    Usage =
     'Usage: rasterize.js URL filename [width] [height] [zoom]' +
     '  e.g., http://example.com output.pdf 1024 768 1.0';
 
@@ -14,10 +12,10 @@ if (system.args.length < 3 || system.args.length > 6) {
     phantom.exit(1);
 }
 
-address = system.args[1];
-output = system.args[2];
-pageW = system.args[3] ? system.args[3] : 1024;
-pageH = system.args[4] ? system.args[4] :  768;
+var address = system.args[1];
+var output = system.args[2];
+var pageW = system.args[3] ? system.args[3] : 1024;
+var pageH = system.args[4] ? system.args[4] :  768;
 page.zoomFactor = system.args[5] ? system.args[5] : 1;
 
 page.viewportSize = { width: pageW, height: pageH };
